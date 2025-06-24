08befa4466a6b7c01c43f6df3d08d45ad5da1b7e
verarr <verarr@fvnki.town>
2025-06-24 04:32:57 +0200

    get a logger

--- common/src/main/java/xyz/verarr/customweathercycle/CustomWeatherCycle.java
+++ common/src/main/java/xyz/verarr/customweathercycle/CustomWeatherCycle.java
@@ -1,7 +1,12 @@
 package xyz.verarr.customweathercycle;
 
+import dev.architectury.event.events.common.CommandRegistrationEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class CustomWeatherCycle {
     public static final String MOD_ID = "customweathercycle";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
         // Write common init code here.
